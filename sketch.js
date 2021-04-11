035a8d85ce561b1c49b7174ab03bab474edf9221
AkankshyaSinha <74859049+AkankshyaSinha@users.noreply.github.com>
2021-04-11 12:34:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,95 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood;
+var foodObj;
+var feed, lastFed
+
+//create feed and lastFed variable here
+
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //create feed the dog button here
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+  feed= createButton("Feed the Dog")
+  feed.position(790,95);
+  feed.mousePressed(feedFoods);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //write code to read fedtime value from the database 
+  var readFedTime= database.ref("FeedTime")
+  //text (lastFed, 54, 52)
+  if (lastFed>=12) {
+    text ("Last Feed : "+ hour()+ "PM", 350, 30)
+  } else if (lastFed===12) {
+    text ("Last Feed : 12 AM", 350, 30)
+  } else {
+    text ("Last Feed : "+ hour()+ "AM", 350, 30)
+  }
+  //write code to display text lastFed time here
+
+ 
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+  dog.addImage(happyDog);
+  if(mousePressed(feed)){
+    deductFood() 
+    readStock(data)
+    getFedTime(lastFed)
+    hour ()
+  }
+  /*updateFoodStock(foodStock)
+  getFedTime(lastFed)*/
+  //write code here to update food stock and last fed time
+
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+
+function feedFoods(){
+  foodS--;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+
